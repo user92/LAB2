55b70ce7c6cba18c2ce55252ff130be224733b5e
user92 <anya.mymail@mail.ru>
2013-05-10 18:36:33 +0400

    Update secondPower.java

--- secondPower.java
+++ secondPower.java
@@ -11,8 +11,12 @@ public class SecondPower extends Polynom  {
 		if (!myFile) throw new Exception; 
 		if (index > file.length(fileName)) throw new Exception;
 		try {
-			
-			
+			for (int i = index; i < 3; ++i) {
+    		        FileReader fileReader[i] = new FileReader(myFile.getAbsolutePath());
+    		        fileReader[i].read();
+        	        fileReader[i].close();
+			}
+			catch Exception() { 
 		}
 			
 		
