b8956344485d5a7e2382536a0d3c666d5ca93675
user92 <anya.mymail@mail.ru>
2013-05-10 12:12:39 +0400

    Create secondPower.java

---
+++ secondPower.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
+
+public class secondPower extends Polynom  {
+  File myFile = new File("myFile");
+	
+	
+	public secondPower (int index, String fileName) throws /*IOException*/ getLastError() {
+		if (/* файла нет */) throw new getLastError(1); 
+		if (index > file.length(fileName)) throw getLastError(2);
+		try {
+			
+			
+		}
+			
+		
+	}
+	
+	
+	
