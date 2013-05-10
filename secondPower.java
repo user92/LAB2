70d69b59946fe2b7d86d4bc0bc8e8113495b61e5
user92 <anya.mymail@mail.ru>
2013-05-10 19:33:05 +0400

    Update secondPower.java

--- secondPower.java
+++ secondPower.java
@@ -18,9 +18,24 @@ public class SecondPower extends Polynom  {
     		        fileReader[i].read();
         	        fileReader[i].close();
 			}
-		catch Exception() { 
+		catch Exception { 
 		}
-		}		
+		}	
+		
+	double GetRoot1 () {
+    	double a = coefficients[2];
+    	double b = coefficients[1];
+    	double c = coefficients[0];
+    	double D = Math.sqrt(b) - 4*a*c;
+    	double discriminant = Math.sqrt(D);
+    	if (D < 0) 
+    		throw new Exception e;
+    	try {
+    		x1 = (-b - discriminant)/(2*a);
+    	} catch (Exception e) { getLastError(9); } 
+  	 
+    return x1;
+}
 		
 	}
 	
