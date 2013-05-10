4d07b96d5b6766c436b80a22d86856a56846d157
user92 <anya.mymail@mail.ru>
2013-05-10 20:16:49 +0400

    Update secondPower.java

--- secondPower.java
+++ secondPower.java
@@ -1,43 +1,71 @@
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 
-public class SecondPower extends Polynom  {
+public class secondPower extends Polynom   {
+  double x1, x2;
+  public secondPower (int index, String fileName) throws IOException {
+	ByteBuffer buffer = ByteBuffer.allocate(4).order(ByteOrder.nativeOrder());
+    File myFile = new File(fileName);
+    if (!myFile) {  
+      getLastError(1);
+      }
+    if (index > myFile.length()) 
+      getLastError(2);
+    try {
+    	for (int i = 0; i < 3; ++i) {
+    		FileReader fileReader = new FileReader(myFile.getAbsolutePath());
+    		fileReader[i].read();
+    		fileReader[i].close();
+    	}    	
+        } catch (Exception e) { }
+        
+        }
+    	
+    
+      
   
-	
-	
-	public SecondPower (int index, String fileName)  {
-		File myFile = new File("myFile");
-		if (!myFile) 
-			throw new Exception; 
-		if (index > file.length(fileName)) 
-			throw new Exception;
-		try {
-			for (int i = index; i < 3; ++i) {
-    		        FileReader fileReader[i] = new FileReader(myFile.getAbsolutePath());
-    		        fileReader[i].read();
-        	        fileReader[i].close();
-			}
-		catch Exception { 
-		}
-		}	
-		
-	double GetRoot1 () {
+    
+    double GetRoot1 () {
     	double a = coefficients[2];
     	double b = coefficients[1];
     	double c = coefficients[0];
     	double D = Math.sqrt(b) - 4*a*c;
     	double discriminant = Math.sqrt(D);
     	if (D < 0) 
-    		throw new Exception e;
+    		throw new MathException e;
     	try {
     		x1 = (-b - discriminant)/(2*a);
     	} catch (Exception e) { getLastError(9); } 
   	 
     return x1;
 }
-		
+    
+    double GetRoot2 () {
+    	double a = coefficients[2];
+    	double b = coefficients[1];
+    	double c = coefficients[0];
+    	double D = Math.sqrt(b) - 4*a*c;
+    	double discriminant = Math.sqrt(D);
+    	try {
+    		x1 = (-b + discriminant)/(2*a);
+    	} catch (Exception e) { getLastError(9); } 
+  	 
+    return x2;
+}
+    
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+	
+
 	}
-	
-	
-	
+
+}
+
