c829244698c1a5f7f5bc1746446a36a53c356fae
user92 <anya.mymail@mail.ru>
2013-05-20 14:28:57 +0400

    Update secondPower.java
    Наследуемый класс - "Квадратное уравнение"

--- secondPower.java
+++ secondPower.java
@@ -1,71 +1,99 @@
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
+public class secondPower extends Polynom {
+	//public static secondPower rez;
+	double x1, x2;
 
 
-public class secondPower extends Polynom   {
-  double x1, x2;
-  public secondPower (int index, String fileName) throws IOException {
-	ByteBuffer buffer = ByteBuffer.allocate(4).order(ByteOrder.nativeOrder());
-    File myFile = new File(fileName);
-    if (!myFile) {  
-      getLastError(1);
-      }
-    if (index > myFile.length()) 
-      getLastError(2);
-    try {
-    	for (int i = 0; i < 3; ++i) {
-    		FileReader fileReader = new FileReader(myFile.getAbsolutePath());
-    		fileReader[i].read();
-    		fileReader[i].close();
-    	}    	
-        } catch (Exception e) { }
-        
-        }
-    	
-    
-      
-  
-    
-    double GetRoot1 () {
-    	double a = coefficients[2];
-    	double b = coefficients[1];
-    	double c = coefficients[0];
+	public secondPower (int deg) {
+		coeff = new double[deg];
+		for (int i = 0; i < deg ; i++) 
+			coeff[i]=0;
+	}
+	
+	
+	
+	public secondPower () {
+		n = 3;
+		int i;
+		coeff = new double[n];
+		for (i = 0; i < n ; i++) 
+			coeff[i]=1;
+		}
+	
+	
+	public secondPower multiply(secondPower b) {
+		secondPower a = this;
+		secondPower rez = new secondPower(a.coeff.length+b.coeff.length);
+		for ( int i = 0; i < a.coeff.length; i++) {
+			for (int j = 0; j < b.coeff.length; j++) {
+				rez.coeff[i + j] += a.coeff[i] * b.coeff[j];
+			}
+		} 
+		return rez;
+	}
+	
+	
+	/*public secondPower divide(secondPower b) throws Exception{
+		int maxDegree = Math.max(this.degree(), b.degree());
+		int minDegree = Math.min(this.degree(), b.degree());
+		int rezDegree = maxDegree - minDegree;
+		int j = minDegree;
+		if (b.isNull()) {
+			throw new Exception("division by zero");
+			} 
+		if (this.degree() < b.degree()) {
+			return null; // нулевой полином
+		}
+		if (this.degree() > b.degree()) {
+			for (int i = maxDegree; i > -1; i--) {
+				for (int k = minDegree; k > -1; k--) {
+					rez.coeff[k] = this.coeff[i]/b.coeff[j];
+		*/			
+					
+
+	
+    double GetRoot1 () throws Exception {
+    	double a = coeff[2];
+    	double b = coeff[1];
+    	double c = coeff[0];
     	double D = Math.sqrt(b) - 4*a*c;
     	double discriminant = Math.sqrt(D);
     	if (D < 0) 
-    		throw new MathException e;
+    		throw new Exception("nothing to calculate");
     	try {
     		x1 = (-b - discriminant)/(2*a);
-    	} catch (Exception e) { getLastError(9); } 
+    	} catch (Exception e) { getLastError(e); } 
   	 
     return x1;
 }
     
     double GetRoot2 () {
-    	double a = coefficients[2];
-    	double b = coefficients[1];
-    	double c = coefficients[0];
+    	double a = coeff[2];
+    	double b = coeff[1];
+    	double c = coeff[0];
     	double D = Math.sqrt(b) - 4*a*c;
     	double discriminant = Math.sqrt(D);
     	try {
     		x1 = (-b + discriminant)/(2*a);
-    	} catch (Exception e) { getLastError(9); } 
+    	} catch (Exception e) { getLastError(e); } 
   	 
     return x2;
 }
     
+	public void getPrint(){
+	int i;
+	for (i = 0; i < n ; i++) {
+		System.out.println(coeff[i]);
+		}
+}
 	
-	
-	
-	
+    public static void getLastError (Exception lastError) {
+		System.out.println("last error is" + lastError.getMessage()); // return lastError; 
+	}
 	
 	public static void main(String[] args) {
-	
-
+		secondPower test = new secondPower();
+		secondPower test2 = new secondPower();
+		test.summa(test2);	
 	}
 
 }
-
